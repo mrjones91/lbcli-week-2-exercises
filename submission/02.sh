1ee7ce77c4014a849a2724bc23ffb4a3389d9e0b
DJ <dj@code-crew.org>
2025-03-27 12:39:27 -0500

    bech32

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
-change_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getrawchangeaddress legacy)
+change_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getrawchangeaddress)
 echo $change_ADDR
