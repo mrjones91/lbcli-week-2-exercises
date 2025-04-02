0f4537019c80cde4e212ac751ecc3783f186c0e6
DJ <dj@code-crew.org>
2025-04-02 13:42:18 -0500

    03

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,6 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype p2sh-segwit)
+segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
 bitcoin-cli -regtest sendtoaddress $segwit_ADDR .0021
 echo $segwit_ADDR
