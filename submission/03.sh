1f0d9bb378c6659473f99bd5c376abf95bb2b262
DJ <dj@code-crew.org>
2025-04-02 13:47:42 -0500

    03

--- submission/03.sh
+++ submission/03.sh
@@ -2,5 +2,5 @@
 # Add funds to the address.
 # Return only the Address
 segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
-bitcoin-cli -regtest sendtoaddress $segwit_ADDR 21 -fallbackfee
+bitcoin-cli -regtest -named sendtoaddress address="$segwit_ADDR" amount=21 fee_rate=2
 echo $segwit_ADDR
