f89f01464bb37af11e02db9f6834c15283b46fe4
DJ <dj@code-crew.org>
2025-04-03 16:25:01 -0500

    idrk. ;n; w reggie

--- submission/03.sh
+++ submission/03.sh
@@ -1,9 +1,9 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-funds=$(bitcoin-cli getnewaddress -addresstype bech32)
-bitcoin-cli generatetoaddress 25 $funds
-segwit_ADDR=$(bitcoin-cli getnewaddress -addresstype bech32)
-bitcoin-cli -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200
-bitcoin-cli generatetoaddress 25 $funds
+funds=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
+bitcoin-cli -regtest generatetoaddress 25 $funds
+segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
+bitcoin-cli -regtest -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200
+bitcoin-cli -regtest generatetoaddress 25 $funds
 echo $segwit_ADDR
