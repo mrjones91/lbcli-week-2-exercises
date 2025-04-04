af6b43af409b1e8e52c51d9b9b50af6927494620
DJ <dj@code-crew.org>
2025-04-04 00:11:17 -0500

    here we gooo

--- submission/03.sh
+++ submission/03.sh
@@ -1,16 +1,16 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-# funds=$(bitcoin-cli getnewaddress "" "bech32")
-# bitcoin-cli generatetoaddress 100 $funds
-segwit_ADDR=$(bitcoin-cli getnewaddress -addresstype bech32)
-txid=$(bitcoin-cli -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200)
-# txid=$(bitcoin-cli sendtoaddress $segwit_ADDR 5 "" "" false true 1 "unset" false)
-# bitcoin-cli generatetoaddress 25 $funds
+# funds=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+# bitcoin-cli -regtest generatetoaddress 100 $funds
+segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
+txid=$(bitcoin-cli -regtest -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200)
+# txid=$(bitcoin-cli -regtest sendtoaddress $segwit_ADDR 5 "" "" false true 1 "unset" false)
+# bitcoin-cli -regtest generatetoaddress 25 $funds
 # echo $txid
-bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction $txid) | jq -r '.vout[0].scriptPubKey.address'
-# ADD=$(bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction $txid) | jq -r '.vout[0].scriptPubKey.address')
-# BALANCE=$(bitcoin-cli getreceivedbyaddress "$ADD" 0)
+bitcoin-cli -regtest decoderawtransaction $(bitcoin-cli -regtest getrawtransaction $txid) | jq -r '.vout[0].scriptPubKey.address'
+# ADD=$(bitcoin-cli -regtest decoderawtransaction $(bitcoin-cli -regtest getrawtransaction $txid) | jq -r '.vout[0].scriptPubKey.address')
+# BALANCE=$(bitcoin-cli -regtest getreceivedbyaddress "$ADD" 0)
 #           if (( $(echo "$BALANCE > 0" | bc -l) )); then
 #             echo "✅ Success: Adding funds to SegWit address passed!"
 #           else
