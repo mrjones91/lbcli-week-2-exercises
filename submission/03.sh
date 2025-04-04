2fe847bb255977b6d6814095738322f2460b6f4f
DJ <dj@code-crew.org>
2025-04-03 22:07:17 -0500

    idrk. ;n; w reggie

--- submission/03.sh
+++ submission/03.sh
@@ -1,9 +1,9 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-funds=$(bitcoin-cli -regtest getnewaddress "" "bech32")
-bitcoin-cli -regtest generatetoaddress 100 $funds
-segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
-bitcoin-cli -regtest -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200
-bitcoin-cli -regtest generatetoaddress 25 $funds
-echo $segwit_ADDR
+# funds=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+# bitcoin-cli -regtest generatetoaddress 100 $funds
+segwit_ADDR=$(bitcoin-cli getnewaddress -addresstype bech32)
+txid=$(bitcoin-cli -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200)
+# bitcoin-cli -regtest generatetoaddress 25 $funds
+bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction $txid) | jq -r '.vout[1].scriptPubKey.address'
