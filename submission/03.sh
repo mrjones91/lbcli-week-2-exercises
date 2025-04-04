03078ea9d2ee9fd61ca5009abb286fff99e99dab
DJ <dj@code-crew.org>
2025-04-03 22:08:08 -0500

    idrk. ;n; w reggie

--- submission/03.sh
+++ submission/03.sh
@@ -3,7 +3,7 @@
 # Return only the Address
 # funds=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # bitcoin-cli -regtest generatetoaddress 100 $funds
-segwit_ADDR=$(bitcoin-cli getnewaddress -addresstype bech32)
-txid=$(bitcoin-cli -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200)
+segwit_ADDR=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
+txid=$(bitcoin-cli -regtest -named sendtoaddress address="$segwit_ADDR" amount=5 fee_rate=200)
 # bitcoin-cli -regtest generatetoaddress 25 $funds
-bitcoin-cli decoderawtransaction $(bitcoin-cli getrawtransaction $txid) | jq -r '.vout[1].scriptPubKey.address'
+bitcoin-cli -regtest decoderawtransaction $(bitcoin-cli -regtest getrawtransaction $txid) | jq -r '.vout[1].scriptPubKey.address'
