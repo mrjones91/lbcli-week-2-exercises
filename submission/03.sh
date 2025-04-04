caf88205232e3faf4bebc387efa24e83dafeb83c
DJ <dj@code-crew.org>
2025-04-04 01:29:54 -0500

    prayer

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,7 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
-bitcoin-cli -regtest gettransaction $(bitcoin-cli -regtest sendtoaddress $(bitcoin-cli -regtest getnewaddress "" "bech32") 0.00005) true | jq -r '.details.[0].address'
+bitcoin-cli -regtest gettransaction $(bitcoin-cli -regtest -named sendtoaddress address="$(bitcoin-cli -regtest getnewaddress "" "bech32")" amount=0.00005 fee_rate=20) true | jq -r '.details.[0].address'
 exit 0
 # funds=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # bitcoin-cli -regtest generatetoaddress 100 $funds
